bf205085141c863f0cebda44f496581d27ba65e9
Lewis Lakerink <lewis@retsim.com>
2017-05-01 16:00:58 +1000

    Initial threading example using Prime number checks

---
+++ BridgeThreadingExample/Main.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using Bridge;
+using Bridge.Html5;
+using Bridge.Linq;
+
+namespace Threading
+{
+    public class Main
+    {
+        // The range of numbers to test for primes (0 - n)
+        private const int MaxNumber = 10000000;
+        // If the first 1000 primes from each run should be printed
+        private const bool PrintFirst1000Primes = false;
+        // The list of ints representing the number of threads to execute in each run
+        private static readonly List<int> NumberOfThreadsPerTest = new List<int> {1, 2, 4, 6, 8, 12, 16};
+
+        // Used to pass the range of ints to the RunPrime thread
+        [ObjectLiteral]
+        private class PrimeRange
+        {
+            // The first int in the range
+            public int First;
+            // The last int in the range
+            public int Last;
+        }
+
+        // Checks if a single number is prime
+        private static bool IsPrime(int number)
+        {
+            if (number == 1) return false;
+            if (number == 2) return true;
+
+            var boundary = (int)Math.Floor(Math.Sqrt(number));
+
+            for (var i = 2; i <= boundary; ++i)
+            {
+                if (number % i == 0)  return false;
+            }
+
+            return true;
+        }
+
+        // The entry point of each thread
+        private static object RunPrime(object param)
+        {
+            // Convert the parameter to a range object
+            var range = (PrimeRange) param;
+            // Create an array to store the list of prime numbers found in the range
+            var primes = new List<int>();
+            // Iterate over the range and add any prime numbers to the primes array
+            for (var n = range.First; n < range.Last; n++)
+            {
+                // Check if this number is prime
+                if (IsPrime(n))
+                    // Yes, add the number to the array
+                    primes.Add(n);
+            }
+
+            // Return the prime numbers
+            return primes.ToArray();
+        }
+
+        // Need to add a thread pool to Bridge perhaps
+        // Simple utility function to join all threads in an array.
+        private static void JoinAll(IEnumerable<Thread> threads, Action onJoined)
+        {
+            // Check if any threads are still alive
+            if (threads.Any(e => e.IsAlive))
+                // Yes, check again in a few milliseconds
+                Window.SetTimeout(() => JoinAll(threads, onJoined), 10);
+            else
+                // No, all threads are finished, trigger the callback
+                onJoined();
+        }
+
+        // Runs the next primes benchmark
+        private static void BenchmarkPrimes()
+        {
+            // Get the number of threads for this benchmark
+            var numberOfThreads = NumberOfThreadsPerTest[0];
+            // Remove this number of threads from the available runs
+            NumberOfThreadsPerTest.RemoveAt(0);
+
+            Console.WriteLine("Starting next benchmark with " + numberOfThreads + " thread(s)...");
+
+            // Get the start time
+            var startTime = DateTime.Now;
+
+            // Create an array to store the prime numbers
+            var primeNumbers = new List<int>();
+            // Calculate how many ints each thread will check
+            var countPerThread = MaxNumber / numberOfThreads;
+            // Create an array to store the created threads
+            var threads = new List<Thread>();
+
+            // Spawn the threads
+            for (var i = 0; i < numberOfThreads; i++)
+            {
+                // Create the range for this thread
+                var range = new PrimeRange
+                {
+                    First = i * countPerThread + 1,
+                    Last = i * countPerThread + countPerThread + 1
+                };
+
+                // Create the new thread
+                var t = new Thread(
+                    // Specify the source files
+                    new[]
+                    {
+                        // Include this javascript file (since bridge is loaded automatically)
+                        Thread.GetCurrentJsFilePath()
+                    }
+                );
+
+                // Start the thread
+                t.Start(
+                    // Set the entry point to RunPrime
+                    RunPrime,
+                    // range is the parameter sent to the entry point
+                    range,
+                    // Set the completion callback when the thread returns a value (thread, original parameter, result)
+                    (thread, param, result) =>
+                    {
+                        // Cast the result object to an int array
+                        var resultPrimes = (int[]) result;
+                        // Add the primes to the list of primes
+                        primeNumbers.AddRange(resultPrimes);
+                    }
+                );
+
+                // Add the created thread to the list of spawned threads
+                threads.Add(t);
+            }
+
+            // Wait for all threads to join
+            JoinAll(threads, () =>
+            {
+                // Get the end time
+                var endTime = DateTime.Now;
+
+                // Clean up all created threads
+                threads.ForEach(e => e.Dispose());
+
+                // Get the number of prime numbers found
+                var primeNumberCount = primeNumbers.Count;
+
+                if (PrintFirst1000Primes)
+                {
+                    // Need to sort the primes array, since it is not in order
+                    primeNumbers = primeNumbers.Slice(0, 1000);
+                    primeNumbers.Sort();
+                    // Print 100 rows of 10 primes
+                    for (var i = 0; i < 100; i++)
+                    {
+                        var s = "";
+                        for (var j = 0; j < 10; j++)
+                            s += primeNumbers[i * 10 + j] + " ";
+
+                        Console.WriteLine(s);
+                    }
+                }
+
+                // Report the statistics from this run
+                Console.WriteLine("Max number: " + MaxNumber + ", Threads: " + numberOfThreads + ", Time taken: " + (endTime - startTime).TotalMilliseconds + "ms, Number of primes: " + primeNumberCount);
+
+                // Check if there are any more runs to do
+                if (NumberOfThreadsPerTest.Count > 0)
+                    // Yes, run the next benchmark
+                    BenchmarkPrimes();
+                else
+                    // No, all done
+                    Console.WriteLine("Complete.");
+            });
+        }
+
+        private static void RunBenchmarkOnMainThread()
+        {
+            Console.WriteLine("Starting baseline benchmark on main thread...");
+
+            // Get the start time
+            var startTime = DateTime.Now;
+
+            var primeNumbers = ((int[]) RunPrime(new PrimeRange
+            {
+                First = 1,
+                Last = MaxNumber + 1
+            })).ToList();
+
+            // Get the end time
+            var endTime = DateTime.Now;
+
+            // Get the number of prime numbers found
+            var primeNumberCount = primeNumbers.Count;
+
+            if (PrintFirst1000Primes)
+            {
+                // Need to sort the primes array, since it is not in order
+                primeNumbers = primeNumbers.Slice(0, 1000);
+                primeNumbers.Sort();
+                // Print 100 rows of 10 primes
+                for (var i = 0; i < 100; i++)
+                {
+                    var s = "";
+                    for (var j = 0; j < 10; j++)
+                        s += primeNumbers[i * 10 + j] + " ";
+
+                    Console.WriteLine(s);
+                }
+            }
+
+            // Report the statistics from this run
+            Console.WriteLine("Max number: " + MaxNumber + ", Threads: (Main Thread), Time taken: " + (endTime - startTime).TotalMilliseconds + "ms, Number of primes: " + primeNumberCount);
+        }
+
+        [Ready]
+        public static void Start()
+        {
+            // Run the baseline benchmark
+            RunBenchmarkOnMainThread();
+
+            // Run the first benchmark
+            BenchmarkPrimes();
+        }
+
+
+    }
+}
---
+++ BridgeThreadingExample/docs/Threading.js
@@ -0,0 +1,206 @@
+/**
+ * @version 1.0.0.0
+ * @copyright Copyright ©  2017
+ * @compiler Bridge.NET 16.0.0
+ */
+Bridge.assembly("Threading", function ($asm, globals) {
+    "use strict";
+
+    Bridge.define("Threading.Main", {
+        statics: {
+            fields: {
+                MaxNumber: 10000000,
+                PrintFirst1000Primes: false,
+                NumberOfThreadsPerTest: null
+            },
+            ctors: {
+                init: function () {
+                    this.NumberOfThreadsPerTest = $asm.$.Threading.Main.f1(new (System.Collections.Generic.List$1(System.Int32))());
+                    Bridge.ready(this.Start);
+                }
+            },
+            methods: {
+                IsPrime: function (number) {
+                    if (number === 1) {
+                        return false;
+                    }
+                    if (number === 2) {
+                        return true;
+                    }
+
+                    var boundary = Bridge.Int.clip32(Math.floor(Math.sqrt(number)));
+
+                    for (var i = 2; i <= boundary; i = (i + 1) | 0) {
+                        if (number % i === 0) {
+                            return false;
+                        }
+                    }
+
+                    return true;
+                },
+                RunPrime: function (param) {
+                    // Convert the parameter to a range object
+                    var range = param;
+                    // Create an array to store the list of prime numbers found in the range
+                    var primes = new (System.Collections.Generic.List$1(System.Int32))();
+                    // Iterate over the range and add any prime numbers to the primes array
+                    for (var n = range.First; n < range.Last; n = (n + 1) | 0) {
+                        // Check if this number is prime
+                        if (Threading.Main.IsPrime(n)) {
+                            primes.add(n);
+                        }
+                    }
+
+                    // Return the prime numbers
+                    return primes.toArray();
+                },
+                JoinAll: function (threads, onJoined) {
+                    // Check if any threads are still alive
+                    if (System.Linq.Enumerable.from(threads).any($asm.$.Threading.Main.f2)) {
+                        window.setTimeout(function () {
+                            Threading.Main.JoinAll(threads, onJoined);
+                        }, 10);
+                    } else {
+                        onJoined();
+                    }
+                },
+                BenchmarkPrimes: function () {
+                    // Get the number of threads for this benchmark
+                    var numberOfThreads = Threading.Main.NumberOfThreadsPerTest.getItem(0);
+                    // Remove this number of threads from the available runs
+                    Threading.Main.NumberOfThreadsPerTest.removeAt(0);
+
+                    Bridge.Console.log("Starting next benchmark with " + numberOfThreads + " thread(s)...");
+
+                    // Get the start time
+                    var startTime = new Date();
+
+                    // Create an array to store the prime numbers
+                    var primeNumbers = new (System.Collections.Generic.List$1(System.Int32))();
+                    // Calculate how many ints each thread will check
+                    var countPerThread = (Bridge.Int.div(Threading.Main.MaxNumber, numberOfThreads)) | 0;
+                    // Create an array to store the created threads
+                    var threads = new (System.Collections.Generic.List$1(System.Threading.Thread))();
+
+                    // Spawn the threads
+                    for (var i = 0; i < numberOfThreads; i = (i + 1) | 0) {
+                        (function () {
+                            // Create the range for this thread
+                            var range = { First: ((((i * countPerThread) | 0) + 1) | 0), Last: ((((((i * countPerThread) | 0) + countPerThread) | 0) + 1) | 0) };
+
+                            // Create the new thread
+                            var t = new System.Threading.Thread(System.Array.init([System.Threading.Thread.getCurrentJsFilePath()], System.String));
+
+                            // Start the thread
+                            t.start('Threading.Main.RunPrime', range, function (thread, param, result) {
+                                    // Cast the result object to an int array
+                                    var resultPrimes = Bridge.cast(result, System.Array.type(System.Int32));
+                                    // Add the primes to the list of primes
+                                    primeNumbers.addRange(resultPrimes);
+                                });
+
+                            // Add the created thread to the list of spawned threads
+                            threads.add(t);
+                        }).call(this);
+                    }
+
+                    // Wait for all threads to join
+                    Threading.Main.JoinAll(threads, function () {
+                        // Get the end time
+                        var endTime = new Date();
+                        Bridge.Linq.Enumerable.from(threads).forEach($asm.$.Threading.Main.f3);
+
+                        // Get the number of prime numbers found
+                        var primeNumberCount = primeNumbers.Count;
+
+                        if (Threading.Main.PrintFirst1000Primes) {
+                            // Need to sort the primes array, since it is not in order
+                            primeNumbers = primeNumbers.slice(0, 1000);
+                            primeNumbers.sort();
+                            // Print 100 rows of 10 primes
+                            for (var i1 = 0; i1 < 100; i1 = (i1 + 1) | 0) {
+                                var s = "";
+                                for (var j = 0; j < 10; j = (j + 1) | 0) {
+                                    s = System.String.concat(s, (primeNumbers.getItem(((((i1 * 10) | 0) + j) | 0)) + " "));
+                                }
+
+                                Bridge.Console.log(s);
+                            }
+                        }
+
+                        // Report the statistics from this run
+                        Bridge.Console.log("Max number: " + Threading.Main.MaxNumber + ", Threads: " + numberOfThreads + ", Time taken: " + System.Double.format((System.DateTime.subdd(endTime, startTime)).getTotalMilliseconds(), 'G') + "ms, Number of primes: " + primeNumberCount);
+
+                        // Check if there are any more runs to do
+                        if (Threading.Main.NumberOfThreadsPerTest.Count > 0) {
+                            Threading.Main.BenchmarkPrimes();
+                        } else {
+                            Bridge.Console.log("Complete.");
+                        }
+                    });
+                },
+                RunBenchmarkOnMainThread: function () {
+                    Bridge.Console.log("Starting baseline benchmark on main thread...");
+
+                    // Get the start time
+                    var startTime = new Date();
+
+                    var primeNumbers = System.Linq.Enumerable.from(Bridge.cast(Threading.Main.RunPrime({ First: 1, Last: 10000001 }), System.Array.type(System.Int32))).toList(System.Int32);
+
+                    // Get the end time
+                    var endTime = new Date();
+
+                    // Get the number of prime numbers found
+                    var primeNumberCount = primeNumbers.Count;
+
+                    if (Threading.Main.PrintFirst1000Primes) {
+                        // Need to sort the primes array, since it is not in order
+                        primeNumbers = primeNumbers.slice(0, 1000);
+                        primeNumbers.sort();
+                        // Print 100 rows of 10 primes
+                        for (var i = 0; i < 100; i = (i + 1) | 0) {
+                            var s = "";
+                            for (var j = 0; j < 10; j = (j + 1) | 0) {
+                                s = System.String.concat(s, (primeNumbers.getItem(((((i * 10) | 0) + j) | 0)) + " "));
+                            }
+
+                            Bridge.Console.log(s);
+                        }
+                    }
+
+                    // Report the statistics from this run
+                    Bridge.Console.log("Max number: " + Threading.Main.MaxNumber + ", Threads: (Main Thread), Time taken: " + System.Double.format((System.DateTime.subdd(endTime, startTime)).getTotalMilliseconds(), 'G') + "ms, Number of primes: " + primeNumberCount);
+                },
+                Start: function () {
+                    // Run the baseline benchmark
+                    Threading.Main.RunBenchmarkOnMainThread();
+
+                    // Run the first benchmark
+                    Threading.Main.BenchmarkPrimes();
+                }
+            }
+        },
+        $entryPoint: true
+    });
+
+    Bridge.ns("Threading.Main", $asm.$);
+
+    Bridge.apply($asm.$.Threading.Main, {
+        f1: function (_o1) {
+            _o1.add(1);
+            _o1.add(2);
+            _o1.add(4);
+            _o1.add(6);
+            _o1.add(8);
+            _o1.add(12);
+            _o1.add(16);
+            return _o1;
+        },
+        f2: function (e) {
+            return e.IsAlive;
+        },
+        f3: function (e) {
+            e.dispose();
+        }
+    });
+});
